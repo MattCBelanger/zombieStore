bcc384a1ecd4bdf9a999ef2b024b1ef1db8728f6
Barney <btong21@gmail.com>
2015-10-23 14:03:42 -0400

    v3

--- app.js
+++ app.js
@@ -1,60 +1,65 @@
-var app = angular.module('ShopApp',['ui.router']);
+var app = angular.module('ShopApp', ['ui.router']);
 
-app.config(function($stateProvider,$httpProvider,$urlRouterProvider){
-	
+app.config(function($stateProvider, $httpProvider, $urlRouterProvider) {
 
-	$urlRouterProvider.otherwise('/home');
 
-	$stateProvider
-	
-	.state('home', {
+    $urlRouterProvider.otherwise('/home');
+
+    $stateProvider
+
+        .state('home', {
         url: '/home',
         templateUrl: 'templates/home.html'
     })
 
     .state('shop', {
         url: '/shop',
-        templateUrl: 'templates/shop.html'
-        controller:'ShopCtrl as Ctrl'
+        templateUrl: 'templates/shop.html',
+        controller: 'ShopCtrl as Ctrl',
+        resolve: {
+            products: function(productService) {
+                return productService.getProducts();
+            }
+        }
     })
 
-	.state('login',{
-		 url: '/login',
-		templateUrl:'templates/login.html',
-		controller:'AuthCtrl as Ctrl'
-	})
-	.state('admin',{
-		url: '/admin',
-		templateUrl:'templates/admin.html',
-		controller:'AdminCtrl as Ctrl',
-		resolve:{
-			path:function($state){
-					if(localStorage.getItem('authToken') == null){
-						console.log(localStorage.getItem('authToken'));
-						$state.go('login');
-					}
-				},
-			products:function(productService){
-					return productService.getProducts();
-				}
-			}
-	})
-	.state('add_product',{
-		url: '/add_product',
-		templateUrl:'templates/add_product.html',
-		controller:'ProductCtrl as Ctrl'
-	});
-	
+    .state('login', {
+            url: '/login',
+            templateUrl: 'templates/login.html',
+            controller: 'AuthCtrl as Ctrl'
+        })
+        .state('admin', {
+            url: '/admin',
+            templateUrl: 'templates/admin.html',
+            controller: 'AdminCtrl as Ctrl',
+            resolve: {
+                path: function($state) {
+                    if (localStorage.getItem('authToken') == null) {
+                        console.log(localStorage.getItem('authToken'));
+                        $state.go('login');
+                    }
+                },
+                products: function(productService) {
+                    return productService.getProducts();
+                }
+            }
+        })
+        .state('add_product', {
+            url: '/add_product',
+            templateUrl: 'templates/add_product.html',
+            controller: 'ProductCtrl as Ctrl'
+        });
 
-	$httpProvider.interceptors.push(function() {
-    return {
-      'request': function(config) {
-        config.headers = config.headers || {};
-        if (localStorage.authToken) {
-          config.headers.Authorization = localStorage.authToken;
-        }
-        return config;
-      }
-    };
-  });
+
+    $httpProvider.interceptors.push(function() {
+        return {
+            'request': function(config) {
+                config.headers = config.headers || {};
+                if (localStorage.authToken) {
+                    config.headers.Authorization = localStorage.authToken;
+                }
+                return config;
+            }
+        };
+    });
 });
---
+++ js/controllers/ShopCtrl.js
@@ -0,0 +1,12 @@
+app.controller('ShopCtrl', ShopCtrl);
+
+function ShopCtrl(productService,$location,products){
+	var self = this;
+
+	//services
+	this.productService = productService;
+	this.products = products;
+	this.search= productService.getProducts;
+	console.log(this.products);
+
+}
