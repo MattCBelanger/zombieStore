a5d84decf03a21970879a4194de000a89aa6e18f
Matt Belanger <matthewcbelanger@gmail.com>
2015-10-26 11:37:48 -0400

    shop

--- js/controllers/ShopCtrl.js
+++ js/controllers/ShopCtrl.js
@@ -1,13 +1,22 @@
 app.controller('ShopCtrl', ShopCtrl);
 
 function ShopCtrl(productService,$location,products){
-	var self = this;
+    var self = this;
 
-	//services
-	this.productService = productService;
-	this.products = products;
-	this.search= productService.getProducts;
-	console.log(this.products);
+    //services
+    this.productService = productService;
+    this.products = products;
+    console.log(this.products);
 
-};
 
+    this.sortOptions = [
+   {label: 'Name', sortField: 'name', reverse: false},
+   {label: 'Price Low - High', sortField: 'price', reverse:false},
+   {label: 'Price High - Low', sortField: 'price', reverse: true},
+    {label: 'Category', sortField: "category", reverse: false}
+ ]
+
+  this.selectedOption= this.sortOptions[0];
+
+
+}
