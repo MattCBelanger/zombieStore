71e5c68a95b8117db9581b26fa3d01a2bd1aaa5e
Matt Belanger <matthewcbelanger@gmail.com>
2015-10-26 15:09:05 -0400

    orders

--- js/controllers/OrderCtrl.js
+++ js/controllers/OrderCtrl.js
@@ -7,35 +7,5 @@ function OrderCtrl(productService,orders){
 	// this.cart=[];
 	
 }
-// OrderCtrl.prototype.addOrder = function(total,tax,finalTotal){
-// 	//create the api request that makes the product on the backend
-// 	//as part of your response you need to add it to your current
-// 	//product array using the product service
-// 	var request_body = {
-// 		cart:this.cart,
-// 		total:total,
-// 		tax:tax,
-// 		final_total:finalTotal
-		
-// 	}
-// 	console.log(request_body);
 
-// 	this.productService.addOrder(request_body);
-
-// }
-
-// OrderCtrl.prototype.addToCart = function(name,price,quantity){
-// 	//create the api request that makes the product on the backend
-// 	//as part of your response you need to add it to your current
-// 	//product array using the product service
-// 	var request_body = {
-// 		name:name,
-// 		price:price,
-// 		quantity:quantity
-	
-// 	}
-
-// 	this.cart.push(request_body);
-// 	console.log(this.cart);
-// }
 
