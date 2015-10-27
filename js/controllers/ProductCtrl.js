7636ce001a8abba4f5c31292b5171f49f8df173c
Matt Belanger <matthewcbelanger@gmail.com>
2015-10-27 09:27:18 -0400

    added prompt to add product page(admin)

--- js/controllers/ProductCtrl.js
+++ js/controllers/ProductCtrl.js
@@ -1,27 +1,27 @@
 app.controller('ProductCtrl',ProductCtrl);
 
-function ProductCtrl(productService){
+function ProductCtrl(productService,$state){
 	this.productService = productService;
 	this.product = this.productService.productID;
+	this.$state=$state;
 	
 }
-// ProductCtrl.prototype.addProduct = function(name,description,price,category,quantity,status){
-// 	//create the api request that makes the product on the backend
-// 	//as part of your response you need to add it to your current
-// 	//product array using the product service
-// 	var request_body = {
-// 		name:name,
-// 		description:description,
-// 		price:price,
-// 		category:category,
-// 		quantity:quantity,
-// 		status:status
-// 	}
+ProductCtrl.prototype.addProduct = function(name,description,price,category,quantity,status){
+	
+	var request_body = {
+		name:name,
+		description:description,
+		price:price,
+		category:category,
+		quantity:quantity,
+		status:status
+	}
 
-// 	this.productService.addProduct(request_body);
+	this.productService.addProduct(request_body);
+	alert("product added!");
+	
 
-
-// }
+}
 
 
 ProductCtrl.prototype.addToCart= function(product){
